dde23ac22627e46580f3737895fbefd129b6e32a
Dylan Jasper <dylanejasper@gmail.com>
2018-07-05 21:20:27 -0700

    chore: added API keys for review

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: config.MY_KEY,
+        mapboxToken: 'pk.eyJ1IjoiZHlsYW5qIiwiYSI6ImNqaWhyYzQ4NDA0cHkzcXF0ZXpzMTNtMXkifQ.gz87sW65RpqcD5RZS8KNvA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
